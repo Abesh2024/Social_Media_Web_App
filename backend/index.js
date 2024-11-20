3194fdd0b3df1405bb70efaa2419f8cefc05bc61
Abesh <abeshsarkar2030@gmail.com>
2024-11-20 11:55:41 +0530

    added

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,6 @@ const corsOptions = {
        optionsSuccessStatus: 200,
        methods: ['GET','HEAD','PUT','PATCH','POST','DELETE'],
 };
-
 app.use(cors(corsOptions));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true }));
