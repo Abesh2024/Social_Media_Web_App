192d51fe841a1a40698e44ee20e64c12bb01691a
Abesh <abeshsarkar2030@gmail.com>
2024-11-20 12:58:20 +0530

    remove mongodb text

--- backend/index.js
+++ backend/index.js
@@ -40,10 +40,7 @@ cloudinary.config({
 });
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log('DB connected successfully');
   })
